d3bf4344bb0d7e153c34902fa01d740545749946
AlexanderAbdulla <alexander.amadeus.abdulla@gmail.com>
2018-08-25 15:26:06 -0700

    Starting Vacation Photo -Text 116-

--- meadowlark.js
+++ meadowlark.js
@@ -72,12 +72,14 @@ app.get('/newsletter', function(req, res){
 })
 
 app.post('/process', function(req, res){
-    console.log('Form (from querystring): ' + req.query.form);
-    console.log('CSRF token from hidden form field: ' + req.body._csrf);
-    console.log('Name from visible form field: ' + req.body.name);
-    console.log('Email from visible form field ' + req.body.email);
-    res.redirect(303, '/thank-you');
+  if(req.xhr || req.accepts('json,html')==='json'){
+      res.send({sucess: true});
+  } else {
+      res.redirect(303, '/thank-you');
+  }
 })
+
+
 
 app.get('/', function(req, res){
     res.render('home');
